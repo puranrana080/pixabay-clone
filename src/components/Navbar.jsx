7f5da181adaa9c95069f8a1299364879ae84cffa
Puran Singh Rana <puransinghrana080@gmail.com>
2025-05-21 22:49:11 +0530

    image by category , query

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useContext } from "react";
+import PixabayContext from "../context/PixabayContext";
 
 const Navbar = () => {
+    const {fetchImageByCategory,setQuery}=useContext(PixabayContext)
   return (
-    <div className='container text-center my-3'>
-
-        <button type="button" className="btn btn-outline-primary mx-3">Primary</button>
-<button type="button" className="btn btn-outline-secondary mx-3">Secondary</button>
-<button type="button" className="btn btn-outline-success mx-3">Success</button>
-<button type="button" className="btn btn-outline-danger mx-3">Danger</button>
-<button type="button" className="btn btn-outline-warning mx-3">Warning</button>
-<button type="button" className="btn btn-outline-info mx-3">Info</button>
-<button type="button" className="btn btn-outline-light mx-3">Light</button>
-<button type="button" className="btn btn-outline-dark mx-3">Dark</button>
+    <>
+    <div className="container text-center my-3">
+      <button type="button" onClick={()=>fetchImageByCategory("nature")} className="btn btn-outline-primary mx-3">
+        Nature
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("science")} className="btn btn-outline-warning mx-3">
+        Science
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("computer")} className="btn btn-outline-light mx-3">
+        Computer
+      </button>
+      <button type="button"onClick={()=>fetchImageByCategory("buildings")} className="btn btn-outline-danger mx-3">
+        Buildings
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("sports")} className="btn btn-outline-warning mx-3">
+        Sports
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("transportation")} className="btn btn-outline-info mx-3">
+        Transportation
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("travel")} className="btn btn-outline-light mx-3">
+        Travel
+      </button>
+      <button type="button" onClick={()=>fetchImageByCategory("food")} className="btn btn-outline-primary mx-3">
+        Food
+      </button>
     </div>
-  )
-}
+    <div className="container" style={{width:"800px"}}>
+        <input type="text" onChange={(e)=>setQuery(e.target.value)} className="form-control bg-dark text-light" ></input>
+    </div>
 
-export default Navbar
+    </>
+  );
+};
+
+export default Navbar;
--- src/context/PixabayState.jsx
+++ src/context/PixabayState.jsx
@@ -1,24 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import PixabayContext from './PixabayContext'
+import React, { useEffect, useState } from "react";
+import PixabayContext from "./PixabayContext";
 
 const PixabayState = (props) => {
-    const [imageData,setImageData]=useState([])
-    const api_key="50423030-2b9d4b90d4e6d22e736af52fd"
-useEffect(()=>{
-    const fetchData=async()=>{
-        const api=await fetch(`https://pixabay.com/api/?key=${api_key}&q=london+flowers&image_type=photo&per_page=100`)
-        const data= await api.json()
-        setImageData(data.hits)
-        console.log(data)
-    }
-    fetchData()
+  const [imageData, setImageData] = useState([]);
+  const [query, setQuery] = useState("london");
+  const api_key = "50423030-2b9d4b90d4e6d22e736af52fd";
+  useEffect(() => {
+    const fetchData = async () => {
+      const api = await fetch(
+        `https://pixabay.com/api/?key=${api_key}&q=${query}&image_type=photo&per_page=100`
+      );
+      const data = await api.json();
+      setImageData(data.hits);
+      console.log(data);
+    };
+    fetchData();
+  }, [query]);
 
-
-},[])
+  const fetchImageByCategory = async (cat) => {
+    const api = await fetch(
+      `https://pixabay.com/api/?key=${api_key}&category=${cat}&image_type=photo&per_page=100`
+    );
+    const data = await api.json();
+    setImageData(data.hits);
+    console.log(data);
+  };
 
   return (
-    <PixabayContext.Provider value={{imageData}}>{props.children}</PixabayContext.Provider>
-  )
-}
+    <PixabayContext.Provider
+      value={{ imageData, fetchImageByCategory, setQuery }}
+    >
+      {props.children}
+    </PixabayContext.Provider>
+  );
+};
 
-export default PixabayState
+export default PixabayState;
